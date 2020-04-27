32bc045bd72b24482128935a594ad781fea8b30f
RTGEvoNeon <48188508+RTGEvoNeon@users.noreply.github.com>
2020-04-27 12:43:53 +0300

    Add files via upload

--- moneyCalc/script.js
+++ moneyCalc/script.js
@@ -1,5 +1,6 @@
 const age = prompt("Ваше имя");
 alert(age);
+alert("Приветсвую вас");
 const generateId = () => `calc${Math.round(Math.random() * 1e8).toString(16)}`;
 
 const totalBalance = document.querySelector(".total__balance"),
